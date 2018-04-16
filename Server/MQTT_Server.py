6bb3cd8b57dfa738fd364609171528ea4d720e49
geraldobraz <gban50@gmail.com>
2018-04-15 22:46:31 -0300

    - Servidor respondendo a: AddAlunos, MudarSenha, ApagarAluno e ListarAluno

--- Server/MQTT_Server.py
+++ Server/MQTT_Server.py
@@ -1,5 +1,5 @@
 ######################################################################
-#                            Servidor                                #
+#                       Servidor Certo                               #
 ######################################################################
 
 
@@ -16,9 +16,15 @@ cnx = mysql.connector.connect(user='root', password='senha',
 # Funcoes MySQL
 
 add_Alunos = ("INSERT INTO Alunos "
-              "(NOME,CPF,SENHA) "
-              "VALUES (%s, %s, %s)")
+               "(NOME,CPF,SENHA) "
+               "VALUES (%s, %s, %s)")
 
+update_senha = ("UPDATE Alunos SET SENHA = %s"
+              "WHERE CPF = %s")
+
+# procura_dados = ("SELECT NOME,CPF,SENHA"
+#                  "FROM Alunos"
+#                  "WHERE CPF = %s")
 
 # Funcoes######
 
@@ -31,20 +37,20 @@ def ProcuraSenha(ValorCpf, ValorSenha):
     cursor = cnx.cursor()
     cpf_atual = int(ValorCpf)
     cursor.execute(query, {'cpf_atual': cpf_atual})
+    # print (cursor)
+    # data = cursor.fetchall()
 
     for row in cursor:
         print(row)
-        print(str(row[0]))
+        print (str(row[0]))
         if str(row[0]) == ValorSenha:
-            print("Senha OK")
+            print ("Senha OK")
             validador_senha = True
         else:
             pass
             # validador_senha = False
     cursor.close()
     return validador_senha
-
-
 def ProcuraCpf(ValorCpf):
     cursor = cnx.cursor()
     query = ("SELECT CPF FROM Alunos ")
@@ -54,20 +60,42 @@ def ProcuraCpf(ValorCpf):
     validador_cpf = False
     for row in cursor:
         if (ValorCpf in row):
-            print(row)
-            print("Cpf existe e esta no banco")
+            print (row)
+            print ("Cpf existe e esta no banco")
             validador_cpf = True
         else:
-            print(row)
-            print("Cpf nao eh esse")
+            print (row)
+            print ("Cpf nao eh esse")
 
     cursor.close()
     return validador_cpf
-
+def ProcuraAluno(ValorCpf):
+    procura_dados = ("SELECT NOME,CPF,SENHA "
+                     "FROM Alunos "
+                     "WHERE CPF = %(cpf_atual)s")
+    cursor = cnx.cursor()
+    cpf_atual = int(ValorCpf)
+    cursor.execute(procura_dados, {'cpf_atual': cpf_atual})
+    dados = []
+    for row in cursor:
+        dados = ([row[i] for i in range(0,len(row))])
+    dados ="$".join(dados)
+    cnx.commit()
+    cursor.close()
+    return dados
+def ApagarAluno(ValorCpf):
+    apagar_aluno = ("DELETE FROM Alunos "
+                         "WHERE CPF = %(cpf_atual)s")
+    cursor = cnx.cursor()
+    cpf_atual = int(ValorCpf)
+    cursor.execute(apagar_aluno, {'cpf_atual': cpf_atual})
+    cnx.commit()
+    cursor.close()
+    print ("Apagou o Aluno: ", ValorCpf)
 
 def on_message(client, userdata, message):
-    print("Message received: " + str(message.payload.decode("utf-8")))
-    print("Topic: " + str(message.topic))
+    print ("Message received: " + str(message.payload.decode("utf-8")))
+    print ("Topic: " + str (message.topic) )
 
     if message.topic == "celular/dados":
         print("Dados")
@@ -75,7 +103,7 @@ def on_message(client, userdata, message):
         # Chamar a func que valida o cpf e a senha
 
         # if dado[0] == cpf and dado[1] == senha:
-        if ProcuraCpf(dado[0]) and ProcuraSenha(dado[0], dado[1]):
+        if ProcuraCpf(dado[0]) and ProcuraSenha(dado[0],dado[1]):
             # Resp_Env = "ok" + "%" + "Fem"
             client.publish("celular/dados/resposta", "ok")
         else:
@@ -87,7 +115,7 @@ def on_message(client, userdata, message):
         print(resp)
 
         # Buscar no BD so sexo relacionado com o cpf indicado
-        # ------------------------
+        #------------------------
         if message.payload.decode("utf-8") == "ON":
             # Mandar um comando RGPIO para abrir a porta
             print("Ligou!")
@@ -106,65 +134,66 @@ def on_message(client, userdata, message):
             print("Ligou!")
         if message.payload.decode("utf-8") == "OFF":
             print("Desligou!")
-            # ------------------------
+        #------------------------
 
     if message.topic == "software/Add/validacao/Sw2Serv":
         print("Dados")
         dado = str(message.payload.decode("utf-8")).split("%")
-        print(dado[1])
-        # dado[0]: Nome #dado[1]: CPF #dado[2]: Senha
+        print (dado[1])
+        #dado[0]: Nome #dado[1]: CPF #dado[2]: Senha
         if not ProcuraCpf(dado[1]):
-            print("Cpf Nao existe no BD :)")
-            data_Alunos = (dado[0], dado[1], dado[2])
+            print ("Cpf Nao existe no BD :)")
+            data_Alunos = (dado[0],dado[1],dado[2])
             cursor = cnx.cursor()
             cursor.execute(add_Alunos, data_Alunos)
             cnx.commit()
             cursor.close()
-            client.publish("software/Add/validacao/Serv2Sw", "Valido")
+            client.publish("software/Add/validacao/Serv2Sw","Valido")
             time.sleep(0.5)
             # cnx.close()
-            print("Finalizado!")
+            print ("Finalizado!")
         else:
-            print("CPF existe no Banco")
+            print ("CPF existe no Banco")
             client.publish("software/Add/validacao/Serv2Sw", "Nao Valido")
 
-    if message.topic == "software/cpf/validacao/Env":
-        print(">>Topic: software/cpf/validacao/Env")
-        #     Procurar no BD se esse cpf existe
-        cpf_ = str(message.payload.decode("utf-8"))
-        print(cpf_)
-        if ProcuraCpf(cpf_):
-            client.publish("software/cpf/validacao/Recv", "Existe")
-            print("Cpf existe no BD")
+    if message.topic == "software/Trocar/validacao/Sw2Serv":
+        print("Dados")
+        dado = str(message.payload.decode("utf-8")).split("%")
+        print (dado[0],dado[1])
+        #dado[0]: CPF #dado[1]: Nova Senha
+        if ProcuraCpf(dado[0]):
+            print ("CPF encontrado)")
+            data_Alunos = (dado[1],dado[0])
+            cursor = cnx.cursor()
+            cursor.execute(update_senha, data_Alunos)
+
+            cnx.commit()
+            cursor.close()
+            client.publish("software/Trocar/validacao/Serv2Sw","Valido")
+            time.sleep(0.5)
+            # cnx.close()
+            print ("Finalizado!")
         else:
-            client.publish("software/cpf/validacao/Recv", "Nao Existe")
-            print("Cpf nao existe no BD")
+            print ("CPF existe no Banco")
+            client.publish("software/Trocar/validacao/Serv2Sw", "Nao Valido")
 
-
-    '''
-    if message.topic == "celular/cpf":
-        print(">>Topic: celular/cpf")
-    #     Buscar no mysql
-        if cpf == message.payload.decode("utf-8"):
-            print("Cpf igual")
-            client.publish("celular/cpf/confirmacao", "ok")
-
-    #         Manda uma msg via mqtt para o celular pra dizer que o cpf exite
+    if message.topic == "software/Procura/validacao/Sw2Serv":
+        dado = str(message.payload.decode("utf-8"))
+        if ProcuraCpf(dado):
+            print (type(ProcuraAluno(dado)))
+            client.publish("software/Procura/validacao/Serv2Sw", "Valido$"+ ProcuraAluno(dado))
+            print ("Enviou!")
         else:
-            print("Cpf diferentes")
+            client.publish("software/Procura/validacao/Serv2Sw", "Nao Valido$" + ProcuraAluno(dado))
 
-    if message.topic == "celular/senha":
-        #     Buscar no mysql
-        print(">>Topic: celular/senha")
-        if senha == message.payload.decode("utf-8"):
-            print("Senha Iguais")
-            client.publish("celular/senha/confirmacao", "ok")
+    if message.topic == "software/Apagar/validacao/Sw2Serv":
+        dado = str(message.payload.decode("utf-8"))
+        if ProcuraCpf(dado):
+            ApagarAluno(dado)
+            client.publish("software/Apagar/validacao/Serv2Sw", "Valido")
+            print ("Enviou!")
         else:
-            print("Senha diferentes")
-
-    '''
-
-
+            client.publish("software/Apagar/validacao/Serv2Sw", "Nao Valido")
 
 
 
@@ -172,12 +201,12 @@ def on_message(client, userdata, message):
 client = mqtt.Client("Servidor_Ubunto")
 
 # Conectando ao broker
-print("Conectando em 192.168.1.3 ...")
+print ("Conectando em 192.168.1.2 ...")
 
 client.on_message = on_message
 
 # client.connect("192.168.0.25", 5050)
-client.connect("192.168.1.3", 5050)
+client.connect("192.168.1.2", 5050)
 
 ############## CELULARES ###########
 # FIXME: Se inscrever no canal celular/porta/Masc!!!!
@@ -188,10 +217,10 @@ client.subscribe("celular/senha")
 client.subscribe("celular/dados")
 client.subscribe("software/Add/validacao/Sw2Serv")
 client.subscribe("software/Add/validacao/Serv2Sw")
-# client.subscribe("software/Add_Aluno")
-# client.subscribe("software/Mudar_Senha")
-# client.subscribe("software/cpf/validacao/Env")
-# client.subscribe("software/cpf/validacao/Recv")
+client.subscribe("software/Trocar/validacao/Sw2Serv")
+client.subscribe("software/Procura/validacao/Sw2Serv")
+client.subscribe("software/Apagar/validacao/Sw2Serv")
+
 
 client.loop_forever()
 
